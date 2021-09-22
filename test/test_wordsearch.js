c1d53a7efba6bcc28a4fe4c48f6d93f00a5f712a
Eileen Xue <xue.eileen@gmail.com>
2021-09-22 20:54:43 +0000

    Solved pair programming exercise with Chintu Mathew

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,66 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  // When the word is empty
+  it("should return false if word is empty", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
+
+  // Find a shorter word in the array
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'S', 'T', 'A', 'R', 'R', 'Y'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'STARRY')
+
+    assert.isTrue(result);
+  });
+
+  // When wordsearch is empty
+  it("should return false if wordsearch is empty", function() {
+    const result = wordSearch([
+      []
+    ], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
+  // Reverse Order
+  it("should return true if the word is present in reverse order", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'N', 'E', 'D', 'R', 'A', 'G', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'S', 'T', 'A', 'R', 'R', 'Y'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'GARDEN')
+
+    assert.isTrue(result);
+  });
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
+// PAIR PROGRAMMING EXERCISE
+
+// Transpose Function 
+const transpose = function(matrix) {
+  // Put your solution here
+  let array = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    array.push([]);
+  }
+
+  for (let row = 0; row < matrix.length; row++){
+    for (let col = 0; col < matrix[row].length; col++){
+      array[col][row] = matrix[row][col];
+    }
+  }
+  return array;
+};
+
+// EDIT HERE
 const wordSearch = (letters, word) => { 
+    // When word or wordsearch is empty
+    if(word === '') {
+        return false;
+    }
+    if (letters.length === 0) {
+        return false;
+    }
+
+    // Horizontal
     const horizontalJoin = letters.map(ls => ls.join(''))
+    
+    // Reverse horizontal
+    const reverseJoin = [];
+    horizontalJoin.forEach(element => {
+        reverseJoin.push(element.split('').reverse().join(""));
+    });
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (l of reverseJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
